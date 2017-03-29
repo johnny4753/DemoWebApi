aa6713ab5331cfd6e49aa4e219baf652875063a4
Johnny <johnny4753@gmail.com>
2017-03-29 21:19:44 +0800

    1. add Repository pattern
    2. CloseLazyLoading  & 設定 ReferenceLoopHandling.Ignore; 避免取得關聯物件時發生循環參考
    3. 完成 CustomerController

---
+++ DemoWebApi/Controllers/CustomersController.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.Entity;
+using System.Web.Http;
+using DemoWebApi.Models.Domain;
+using DemoWebApi.Repository;
+
+namespace DemoWebApi.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private Repository<Customer> _customerRepository;
+
+        public CustomersController()
+        {
+            _customerRepository = new Repository<Customer>();
+        }
+
+        
+
+        [Route("api/customers/{id}")]
+        public IHttpActionResult Get(string id)
+        {
+            var customer = _customerRepository.Get(x => x.CustomerID == id);
+            return Ok(customer);
+        }
+
+        [Route("api/customersWithOrder")]
+        public IHttpActionResult GetCustomersWithOrder()
+        {
+            var customers = _customerRepository.GetAll().Include(x => x.Orders);
+            return Ok(customers);
+        }
+
+        [Route("api/customers")]
+        [HttpPost]
+        public IHttpActionResult CreateCustomer([FromBody]Customer customer)
+        {
+            _customerRepository.Create(customer);
+            return Ok(customer);
+        }
+
+        [Route("api/customers/{id}")]
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(string id, [FromBody]Customer customer)
+        {
+            customer.CustomerID = id;
+            var updatedCustomer = _customerRepository.Update(customer);
+            return Ok(updatedCustomer);
+        }
+
+        [Route("api/customers/{id}")]
+        [HttpDelete]
+        public void DeleteCustomer(string id)
+        {
+            _customerRepository.Delete(new Customer {CustomerID = id});
+        }
+    }
+}
---
+++ DemoWebApi/Repository/Repository.cs
@@ -0,0 +1,95 @@
+﻿using DemoWebApi.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DemoWebApi.Repository
+{
+    public class Repository<TEntity> where TEntity : class
+    {
+        public Repository()
+                    : this(new NorthwindDb())
+        {
+        }
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+            CloseLazyLoading();
+        }
+
+        protected DbContext Context { get; set; }
+
+        public void CloseLazyLoading()
+        {
+            Context.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public TEntity Create(TEntity instance)
+        {
+            var entity = Context.Set<TEntity>().Add(instance);
+            SaveChanges();
+            return entity;
+        }
+
+        public void Create(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().AddRange(entities);
+            SaveChanges();
+        }
+
+        public void Delete(TEntity instance)
+        {
+            Context.Entry(instance).State = EntityState.Deleted;
+            SaveChanges();
+        }
+
+        public bool Exist(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().Any(predicate);
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            TEntity entity = Context.Set<TEntity>().FirstOrDefault(predicate);
+            return entity;
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().AsQueryable();
+        }
+
+        public void SaveChanges()
+        {
+            Context.SaveChanges();
+        }
+
+        public TEntity Update(TEntity instance)
+        {
+            Context.Entry(instance).State = EntityState.Modified;
+            SaveChanges();
+            return instance;
+        }
+
+        #region -- dispose context --
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            if (Context == null) return;
+            Context.Dispose();
+            Context = null;
+        }
+
+        #endregion -- dispose context --
+    }
+}
