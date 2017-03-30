0b314a9cfe1c3d23219c0f3e84fe0bbdb17129cb
Johnny <johnny4753@gmail.com>
2017-03-30 19:02:43 +0800

    1. 顧客訂單搜尋 API 2. 使用 LinqKit PredicateBuilder 做可選搜尋條件串接，並把責任賦予 OrderSearchFilter model

--- DemoWebApi/Controllers/CustomersController.cs
+++ DemoWebApi/Controllers/CustomersController.cs
@@ -1,10 +1,12 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Web.Http;
 using System.Web.Http.Description;
 using DemoWebApi.Extension;
 using DemoWebApi.Interface;
+using DemoWebApi.Models;
 using DemoWebApi.Models.Domain;
 using DemoWebApi.Repository;
 
@@ -48,6 +50,28 @@ namespace DemoWebApi.Controllers
             var customers = _customerRepository.GetAll().Include(x => x.Orders);
             return Ok(customers);
         }
+
+        [HttpPost]
+        [Route("api/customers/{customerId}/orders/{year:int}/{month:int}/page/{pageNumber:int=1}")]
+        public IHttpActionResult GetCustomerOrdersBy(string customerId, int year, int month, int pageNumber, OrderSearchFilter searchFilter)
+        {
+            var startTime = new DateTime(year, month, 1);
+            var endTime = new DateTime(year, month + 1, 1);
+            //取得顧客該月訂單
+            var monthOrders = _customerRepository
+                .GetAll()
+                .Include(x => x.Orders)
+                .FirstOrDefault(x => x.CustomerID == customerId)?
+                .Orders
+                .Where(x => x.OrderDate >= startTime
+                            && x.OrderDate < endTime );
+            //搜尋過濾
+            var filteredOrders = monthOrders?.Where(searchFilter.GetPredicate());
+
+            return Ok(filteredOrders);
+        }
+
+        
 
         [Route("api/customersCsv")]
         public IHttpActionResult GetCustomersCsv()
---
+++ DemoWebApi/Models/OrderSearchFilter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq.Expressions;
+using DemoWebApi.Models.Domain;
+using LinqKit;
+
+namespace DemoWebApi.Models
+{
+    /// <summary>
+    /// 訂單搜尋過濾條件
+    /// </summary>
+    public class OrderSearchFilter
+    {
+        /// <summary>
+        /// 最小運費，意即搜尋運費 >= 此值的訂單
+        /// </summary>
+        [Column(TypeName = "money")]
+        public decimal? SearchMinFreight { get; set; }
+
+        /// <summary>
+        /// 船名
+        /// </summary>
+        [StringLength(40)]
+        public string SearchShipName { get; set; }
+
+        /// <summary>
+        /// 船城市
+        /// </summary>
+        [StringLength(15)]
+        public string SearchShipCity { get; set; }
+
+        public ExpressionStarter<Order> GetPredicate()
+        {
+            var predicate = PredicateBuilder.New<Order>(true);
+            if (SearchMinFreight.HasValue)
+            {
+                predicate = predicate.And(x => x.Freight >= SearchMinFreight);
+            }
+            if (!string.IsNullOrEmpty(SearchShipName))
+            {
+                predicate = predicate.And(x => x.ShipName.Contains(SearchShipName));
+            }
+            if (!string.IsNullOrEmpty(SearchShipCity))
+            {
+                predicate = predicate.And(x => x.ShipCity.Contains(SearchShipCity));
+            }
+
+            return predicate;
+        }
+    }
+}
